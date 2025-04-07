9d226d548a063b2fcb6d8d482dbae9b7e276d278
Alozi <alozimozi@gmail.com>
2025-04-07 16:32:53 +0300

    Add changes on the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,15 @@ import Image from "../assets/logo.jpg";
 export default function Header({ cartLength, openCartModal }) {
   return (
     <header id="main-header">
-      <h1 id="title">
-        Food Order App 💪 <img src={Image} alt="Food order logo" />
-      </h1>
-      <button className="button" onClick={openCartModal}>
-        Cart {cartLength > 0 && `(${cartLength})`}
-      </button>
+      <div id="title">
+        <img src={Image} alt="Food order logo" />
+        <h1>Food Order App 💪</h1>
+      </div>
+      <nav>
+        <button className="button" onClick={openCartModal}>
+          Cart {cartLength > 0 && `(${cartLength})`}
+        </button>
+      </nav>
     </header>
   );
 }
